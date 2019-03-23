1fdf0194e1e80990fc7a7c9df613b4120a13cbf5
Michel Bolzon <michel.bolzon123@Gmail.com>
2019-03-22 22:25:57 -0300

    #

--- database/seeds/BankAccountTableSeeder.php
+++ database/seeds/BankAccountTableSeeder.php
@@ -12,28 +12,34 @@ class BankAccountTableSeeder extends Seeder
      */
     public function run()
     {
-        BankAccount::create([
-            'name' => 'Nu Bank',
-            'agency' => '0001',
-            'number_account' => "8229254",
-            'digit_account' => "4",
-            'bank_id' => 96
-        ]);
-        BankAccount::create([
-            'name' => 'Caixa Kakogawa',
-            'agency' => '3531',
-            'operation' => '013',
-            'number_account' => "5093",
-            'digit_account' => "5",
-            'bank_id' => 33
-        ]);
-        BankAccount::create([
-            'name' => 'Caixa Grevilha',
-            'agency' => '3123',
-            'operation' => '013',
-            'number_account' => "18747",
-            'digit_account' => "9",
-            'bank_id' => 33
-        ]);
+        if(BankAccount::where('name', 'Nu Bank')->count() === 0){
+            BankAccount::create([
+                'name' => 'Nu Bank',
+                'agency' => '0001',
+                'number_account' => "8229254",
+                'digit_account' => "4",
+                'bank_id' => 96
+            ]);
+        }
+        if(BankAccount::where('name', 'Caixa Kakogawa')->count() === 0) {
+            BankAccount::create([
+                'name' => 'Caixa Kakogawa',
+                'agency' => '3531',
+                'operation' => '013',
+                'number_account' => "5093",
+                'digit_account' => "5",
+                'bank_id' => 33
+            ]);
+        }
+        if(BankAccount::where('name', 'Caixa Grevilha')->count() === 0) {
+            BankAccount::create([
+                'name' => 'Caixa Grevilha',
+                'agency' => '3123',
+                'operation' => '013',
+                'number_account' => "18747",
+                'digit_account' => "9",
+                'bank_id' => 33
+            ]);
+        }
     }
 }
