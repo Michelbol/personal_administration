175b546888727e6b91be6172f090b4882a3a9332
Michel Bolzon <michel.bolzon123@Gmail.com>
2019-04-06 12:12:06 -0300

    #

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'personal-administrator.com.br'),
+    'url' => env('APP_URL', '35.185.25.201'),
 
     'asset_url' => env('ASSET_URL', null),
 
