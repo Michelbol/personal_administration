18dcdcefb5e7f4005be29aea2b924a9d683e4279
Michel Bolzon <michel.bolzon123@Gmail.com>
2019-04-06 12:05:04 -0300

    #

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '35.227.58.110'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'personal_administrator'),
             'username' => env('DB_USERNAME', 'root'),
