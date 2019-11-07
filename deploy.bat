dbe9d8c22955e6d071847adb200aa6d08fcb963f
Michel Bolzon Souza Dos Reis <michel.bolzon123@gmail.com>
2019-11-06 21:54:09 -0300

    updating the deploybat

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,1 @@
-git checkout dev
-git pull
-git checkout master
-git pull
-git merge dev
-git push
-git checkout dev
-git push
+git checkout dev && git pull && git checkout master && git pull && git merge dev && git push && git checkout dev && git push
