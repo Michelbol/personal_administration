c963a458c638c98e0f1db4ee7bbda736d0133c93
Michel Bolzon Souza Dos Reis <michel.bolzon123@gmail.com>
2020-08-27 23:49:22 -0300

    Add deleted at

--- app/Repositories/ProductSupplierRepository.php
+++ app/Repositories/ProductSupplierRepository.php
@@ -49,6 +49,8 @@ class ProductSupplierRepository
             ::from('product_suppliers as ps')
             ->join('products as p', 'p.id', 'ps.product_id')
             ->join('suppliers as s', 's.id', 'ps.supplier_id')
+            ->where('p.deleted_at', null)
+            ->where('s.deleted_at', null)
             ->get(
                 [
                     $this->queryRawProductName(),
