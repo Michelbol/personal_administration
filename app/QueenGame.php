1246ee488d96c2d1aecc38d2fa521551da8fb969
Michel Bolzon Souza Dos Reis <michel.bolzon123@gmail.com>
2020-10-12 23:18:12 -0300

    #

--- app/QueenGame.php
+++ app/QueenGame.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class QueenGame extends Model
 {
     protected $fillable = [
-        'model'
+        'model',
+        'white_left',
+        'black_left',
+        'difficulty',
+        'start_game',
+        'end_game',
+        'type_white',
+        'type_black',
+        'type_black_machine',
+        'type_white_machine',
     ];
 }
